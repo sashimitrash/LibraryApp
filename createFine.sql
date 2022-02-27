cd9319447d80b25568be114f4fa1215f4638b184
sashimitrash <zuzuazumi@gmail.com>
2022-02-27 15:54:50 +0800

    fine and payment table

--- createFine.sql
+++ createFine.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Fine(
      memberID     VARCHAR(5)  NOT NULL,
      accessionNo  VARCHAR(10) NOT NULL,
-     amount       VARCHAR(10) NOT NULL,
+     amount       INT NOT NULL CHECK (amount > 0),
      PRIMARY KEY (memberID, accessionNo),
+     FOREIGN KEY (accessionNo) REFERENCES Book(accessionNo) ON DELETE RESTRICT,
      FOREIGN KEY (memberID) REFERENCES Member(memberID) ON DELETE RESTRICT);
--- createPayment.sql
+++ createPayment.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Payment(
 	memberID     VARCHAR(5)  NOT NULL,
     accessionNo  VARCHAR(10) NOT NULL,
-    paymentDate VARCHAR(10),
+    paymentDate DATE NOT NULL,
     PRIMARY KEY (memberID, accessionNo, paymentDate),
     FOREIGN KEY (memberID) REFERENCES Member(memberID), 
     FOREIGN KEY (accessionNo) REFERENCES Book(accessionNo));
