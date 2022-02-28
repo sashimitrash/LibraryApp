4d8b9b51eb8c4e0e7ee78b2b627d51b3a5e236ac
sashimitrash <99946541+sashimitrash@users.noreply.github.com>
2022-02-28 18:02:43 +0800

    testing (#2)
    * test
    
    * testing
    
    * fine and payment table

---
+++ createPayment.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Payment(
+	memberID     VARCHAR(5)  NOT NULL,
+    accessionNo  VARCHAR(10) NOT NULL,
+    paymentDate DATE NOT NULL,
+    PRIMARY KEY (memberID, accessionNo, paymentDate),
+    FOREIGN KEY (memberID) REFERENCES Member(memberID), 
+    FOREIGN KEY (accessionNo) REFERENCES Book(accessionNo));
+    
+    
