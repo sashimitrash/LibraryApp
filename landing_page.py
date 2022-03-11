d633f7ca5940283768b65d67d3418bec4616931c
kctey <kaicong12@gmail.com>
2022-03-11 09:57:51 +0800

    fix landing page coordinates

--- landing_page.py
+++ landing_page.py
@@ -20,7 +20,7 @@ class LandingPage(Container):
         self.landing_report_image = self.open_image('apps/resources/reports.png', LANDING_PAGE_ICON_SIZE,
                                                     LANDING_PAGE_ICON_SIZE)
         self.report_button = Button(root, image=self.landing_report_image, command=self.go_to_report)
-        self.report_button.place(relx=0.7, rely=0.7, anchor='center')
+        self.report_button.place(relx=0.75, rely=0.7, anchor='center')
         self.report_text = Label(root, text='Reports', font=(FONT, LANDING_PAGE_FONT_SIZE, STYLE),
                                  fg='black',
                                  bg='white')
@@ -56,36 +56,25 @@ class LandingPage(Container):
                                  bg='white')
         self.member_text.place(relx=0.25, rely=0.45, anchor='center')
 
-        # Membership option
-        self.landing_member_image = self.open_image('apps/resources/reports.png', LANDING_PAGE_ICON_SIZE,
-                                            LANDING_PAGE_ICON_SIZE)
-        self.member_button = Button(root, image=self.landing_report_image, command=self.go_to_member)
-        self.member_button.place(relx=0.2, rely=0.2, anchor='center')
-        self.member_text = Label(root, text='Membership', font=(FONT, LANDING_PAGE_FONT_SIZE, STYLE),
-                                 fg='black',
-                                 bg='white')
-        self.member_text.place(relx=0.2, rely=0.4, anchor='center')
-
-
         # loan option
         self.landing_loan_image = self.open_image('apps/resources/loan.png', LANDING_PAGE_ICON_SIZE,
                                                     LANDING_PAGE_ICON_SIZE)
         self.loan_button = Button(root, image=self.landing_loan_image, command=self.go_to_loan)
-        self.loan_button.place(relx=0.3, rely=0.3, anchor='center')
+        self.loan_button.place(relx=0.75, rely=0.25, anchor='center')
         self.loan_text = Label(root, text='Loans', font=(FONT, LANDING_PAGE_FONT_SIZE, STYLE),
                                  fg='black',
                                  bg='white')
-        self.loan_text.place(relx=0.7, rely=0.9, anchor='center')
+        self.loan_text.place(relx=0.75, rely=0.45, anchor='center')
 
         # reservation option
         self.landing_reservation_image = self.open_image('apps/resources/reservation.png', LANDING_PAGE_ICON_SIZE,
                                                     LANDING_PAGE_ICON_SIZE)
         self.reservation_button = Button(root, image=self.landing_reservation_image, command=self.go_to_reservation)
-        self.reservation_button.place(relx=0.5, rely=0.5, anchor='center')
+        self.reservation_button.place(relx=0.25, rely=0.7, anchor='center')
         self.reservation_text = Label(root, text='Reservations', font=(FONT, LANDING_PAGE_FONT_SIZE, STYLE),
                                  fg='black',
                                  bg='white')
-        self.reservation_text.place(relx=0.7, rely=0.9, anchor='center')
+        self.reservation_text.place(relx=0.25, rely=0.9, anchor='center')
 
     def go_to_report(self):
         Report(self.root, self.parent, self.engine)
